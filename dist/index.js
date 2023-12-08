176f182fe7f38d74a1baba0d960c507216530d61
lucidcoderio <147671842+lucidcoderio@users.noreply.github.com>
2023-12-08 06:35:05 -0700

    csv-writer

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+class CSVWriter {
+    constructor(columns) {
+        this.columns = columns;
+        this.csv = this.columns.join(',') + '\n';
+    }
+    save(filename) {
+        (0, fs_1.appendFileSync)(filename, this.csv);
+        this.csv = '\n';
+        console.log('file saved to', filename);
+    }
+    addRows(values) {
+        let rows = values.map((v) => this.formatRow(v));
+        this.csv += rows.join('\n');
+        console.log(this.csv);
+    }
+    formatRow(p) {
+        return this.columns.map((col) => p[col]).join(',');
+    }
+}
+const writer = new CSVWriter(['id', 'amount', 'to', 'notes']);
+writer.addRows([
+    { id: 1, amount: 50, to: 'Tyler', notes: 'for design work' },
+    { id: 2, amount: 75, to: 'Tyler', notes: 'for web dev work' },
+]);
+writer.save('../data/payments.csv');
